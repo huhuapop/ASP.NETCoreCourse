8cb2d18a7ecc05e918a47008834d03cbb196deb8
huhuapop <huhuapop@outlook.com>
2018-10-28 23:42:13 -0400

    Init

---
+++ myconsoleapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 0;
+            var j = 0; 
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(i+" "+j);
+        }
+    }
+}
